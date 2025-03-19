fbb8c5a8edd1f8fccb011133158fcaa4a5f15da6
liulin35 <liulin35@maoyan.com>
2025-03-19 21:43:22 +0800

    fix: rename

--- src/client/index.d.ts
+++ src/client/index.d.ts
@@ -1,6 +1,6 @@
 import { ChatConfig } from '../config';
 import { IntelligentChat } from './App';
 
-declare module 'my-chat-widget' {
+declare module 'my-chat-widgets' {
   export const IntelligentChat: React.FC<{ config: ChatConfig }>;
 } 
